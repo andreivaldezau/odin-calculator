246e50cf98188a5158c77f899a4efffbead2fa8a
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-29 21:57:24 +1000

    Add numbers and reset button function

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ let a = 0;
 let b = 0;
 let operator = "";
 
+const displayText = document.querySelector(".display-text");
+const numbers = document.querySelectorAll("button.number");
+const reset = document.querySelector("button.reset");
+
+numbers.forEach((button) =>
+  button.addEventListener("click", () => onClickNumber(button.textContent))
+);
+
+reset.addEventListener("click", () => onClickReset());
+
+function onClickNumber(text) {
+  if (displayText.textContent === "0") {
+    displayText.textContent = text;
+  } else {
+    displayText.textContent += text;
+  }
+}
+
+function onClickReset() {
+  displayText.textContent = 0;
+}
+
 function operate(a, b, operator) {
   switch (operator) {
     case "+":
