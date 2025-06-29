695dc11cd5fcdc3225850a0451bd5717e6d60fe0
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-29 20:19:37 +1000

    Add operator functions

---
+++ index.js
@@ -0,0 +1,34 @@
+let a = 0;
+let b = 0;
+let operator = "";
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      console.log("Invalid operation.");
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
